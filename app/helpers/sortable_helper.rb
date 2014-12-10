ab8d70be5c71c0c3f4243c8cdf3eaa2147b53e25
xiongbo <xiongbo027@gmail.com>
2014-12-10 20:42:22 +0800

    add some helper method and test

--- app/helpers/sortable_helper.rb
+++ app/helpers/sortable_helper.rb
@@ -1,3 +1,21 @@
-module SortableHelper
+require 'erb'
+require 'json'
+require 'pry'
 
+module SortableHelper 
+  def list_sortable &block
+    sortable(:ul, &block)
+  end
+
+  def table_sortable &block
+    sortable(:table, &block)
+  end
+
+  private
+  def sortable(node, &block)
+    html = "<#{node.to_s} class=\".sortable\">"
+    html <<  yield(block)
+    html << "</#{node.to_s}>" 
+    html
+  end
 end
--- test/helpers/sortable_helper_test.rb
+++ test/helpers/sortable_helper_test.rb
@@ -2,7 +2,26 @@ require_relative '../../app/helpers/sortable_helper'
 require_relative '../test_helper'
 require 'active_support'
 require 'action_view'
+require 'erb'
 
 class SortableHelperTest < TestCase
   include SortableHelper
+
+  def test_list_sortable
+    expected = %Q(<ul class=".sortable"><li>a</li><li>b</li></ul>)
+    result = list_sortable do 
+      "<li>a</li><li>b</li>"
+    end
+
+    assert_equal expected, result
+  end
+
+  def test_table_sortable
+    expected = %Q(<table class=".sortable"><tr><td></td></tr></table>)
+    result = table_sortable do 
+      "<tr><td></td></tr>"
+    end
+
+    assert_equal expected, result
+  end
 end
